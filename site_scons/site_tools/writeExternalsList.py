a8a00d1a2f71af12a580d44f02e43cbbfe26061f
Joanne Bogart <jrb@slac.stanford.edu>
2017-10-17 17:48:10 -0700

    New option --externalsList for writing file to data dir with name, version of externals to be used

---
+++ site_scons/site_tools/writeExternalsList.py
@@ -0,0 +1,31 @@
+import os, os.path
+import SCons.Action
+import SCons.Builder
+
+def generate(env):
+    # Write a file in $INST_DIR/data directory listing externals
+    # with versions used by this super-package
+    def createExternalsList(target = None, source = None, env = None):
+        fp = open(str(target[0]), 'w')
+        fp.write("# externals required for this super-package \n\n")
+        for ext in env['configuredExternals']:
+            print("writeExternalsList: Processing external " + str(ext["name"]))
+            fp.write("%s-%s" %(ext["name"], ext["version"]))
+            fp.write("\n")
+        fp.close()
+        return 0
+
+
+    def createExternalsListGenerator(source, target, env, for_signature):
+        actions = [env.Action(createExternalsList, "Creating externals list")]
+
+        return actions
+
+    suf = 'extList'
+
+    env.Append(BUILDERS={'GenerateExternalsList' : env.Builder(generator=createExternalsListGenerator,
+                                                         suffix=suf)})
+
+def exists(env):
+    return 1;
+
